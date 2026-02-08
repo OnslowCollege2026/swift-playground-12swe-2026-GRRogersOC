382fed5be582e85be923796d9ab200f577bba1d7
GRRogersOC <george.robionyrogers@student.onslow.school.nz>
2026-02-08 04:01:11 +0000

    Feb 8, 2026, 4:01 AM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,6 +4,34 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
-    }
+        // let roomLength = 6.0
+        // let roomWidth = 4.5
+        // let roomHeight = 2.7
+
+        // let roomArea = roomWidth * roomLength
+        // let roomVolume = roomWidth * roomLength * roomHeight
+
+        print("Enter room length:")
+        if let input = readLine(), let length = Double(input) {
+        
+        print("Enter room width")
+        if let input = readLine(), let width = Double(input){
+        
+        
+        print("Enter room height")
+        if let input = readLine(), let height = Double(input){
+
+        let roomVolume = length * width * height
+        let roomArea = length * width
+
+        print("Room area",roomArea, "m²")
+        print("Room volume", roomVolume, "m³")
+
+        let furnitureVolumes = [1.2, 0.8, 2.5, 0.6, 1.0]
+        let listLength = furnitureVolumes.count
+        
+        for number in 1...5 {
+            print(number) // Prints 1, 2, 3, 4, 5
+        }
+    }  
 }
